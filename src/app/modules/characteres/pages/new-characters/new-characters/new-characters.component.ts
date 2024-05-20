33179c6b833a5cbe50620f8d8e337abfe4b129b2
Brendon Costa <brendoncosta93@gmail.com>
2024-05-20 18:12:45 -0300

    Criando layout da tela de adicionar personagens

--- src/app/modules/characteres/pages/new-characters/new-characters/new-characters.component.ts
+++ src/app/modules/characteres/pages/new-characters/new-characters/new-characters.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import {PoAvatarModule, PoButtonModule, PoPageModule, PoSearchModule, PoWidgetModule} from "@po-ui/ng-components";
+import {PoPageDynamicSearchModule} from "@po-ui/ng-templates";
 
 @Component({
   selector: 'app-new-characters',
   standalone: true,
-  imports: [],
+  imports: [
+    PoPageModule,
+    PoWidgetModule,
+    PoAvatarModule,
+    PoSearchModule,
+    PoPageDynamicSearchModule,
+    PoButtonModule
+  ],
   templateUrl: './new-characters.component.html',
   styleUrl: './new-characters.component.scss'
 })
 export class NewCharactersComponent {
 
+  avatar = 'https://rickandmortyapi.com/api/character/avatar/43.jpeg';
+
+  addCharacter() {
+    console.log('addCharacter');
+  }
+
+  moreCharacter() {
+    console.log('moreCharacter');
+  }
 }
